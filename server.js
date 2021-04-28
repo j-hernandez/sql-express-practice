bc01efbdcec44b071dc7897b3d80fc99ed30e519
James Hernandez <james@j-hernandez.com>
2021-04-27 21:48:48 -0400

    head start

--- server.js
+++ server.js
@@ -1,11 +1,26 @@
+const { response } = require("express");
 const express = require("express");
-const bodyParser = require("body-parser");
 const pgp = require("pg-promise")();
 const db = pgp("postgres://hyeeumfv:1YwH3PINc4cZLU1YqnL2Lix1AtNwci7s@queenie.db.elephantsql.com:5432/hyeeumfv");
 const app = express();
 const PORT = 3000;
 
-app.use(bodyParser.json());
+app.use(express.json());
+app.use(express.urlencoded());
+
+app.get('/users', async (req, res) => {
+  const users = await db.any("SELECT * FROM users").then((users) => {
+    return users;
+  })
+  res.send(users);
+})
+
+app.get('/comments', async (req, res) => {
+  const comments = await db.any("SELECT * FROM comments").then((comments) => {
+    return comments;
+  })
+  res.send(comments);
+})
 
 
 
